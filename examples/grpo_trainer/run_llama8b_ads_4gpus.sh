b14446f604a4648ed21ec605ab050694b6ed5163
Winnie Chow <wychow@stanford.edu>
2025-05-24 17:45:32 -0700

    Fix cpu bug

--- examples/grpo_trainer/run_llama8b_ads_4gpus.sh
+++ examples/grpo_trainer/run_llama8b_ads_4gpus.sh
@@ -36,7 +36,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.logger=['console','wandb'] \
     trainer.project_name='verl_grpo_example_gsm8k' \
     trainer.experiment_name='ads_test' \
-    trainer.n_gpus_per_node=8 \
+    trainer.n_gpus_per_node=4 \
     trainer.nnodes=1 \
     trainer.save_freq=20 \
     trainer.test_freq=5 \
